99259f9e8e2a35c3d2ce594f5331a719cfbfd05e
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-10-22 23:20:45 +0900

    weather API를 이용해 날씨 정보 불러오기

--- js/weather.js
+++ js/weather.js
@@ -1,12 +1,29 @@
-//1번 인자: 성공했을시 실행되는 함수
-//2번 인자: 실패시 실행되는 함수
+
+
+const API_KEY = "4fbccc77c98331be73c86f4ec3302b14";
+
+
 
 function onGeoOk(position) {
+    console.log(position);
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            const weather = document.getElementById("weather");
+            const city = document.getElementById("city");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main}/${Math.round((data.main.temp) - 272.15)}Cº`;
+
+        });
 }
 function onGeoError() {
     alert("Can't find you. No weather for you.")
 }
-navigator.geolocation.getCurrentPosition();
+
+//1번 인자: 성공했을시 실행되는 함수
+//2번 인자: 실패시 실행되는 함수
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
