76811b8d7dae324c96ede0f777afb1e1bbe1f32f
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-10-23 23:01:06 +0900

    채팅창, 검색 input 작동 안됨

---
+++ js/chatmemo.js
@@ -0,0 +1,20 @@
+const chatForm = document.querySelector(".reply_form");
+const chatInput = chatForm.querySelector(".reply_input");
+const button = chatForm.querySelector("button");
+
+
+// function onChatMemoSubmit(event) {
+//     alert("input submit!");
+
+//     event.preventDefualt();
+//     const newMemo = chatInput.value;
+//     chatInput.value = "";
+//     const memoDiv = document.createElement("div");
+
+// }
+// function onButtonClick() {
+//     alert("button Clicked!");
+// }
+
+// chatForm.addEventListener("submit", onChatMemoSubmit);
+// button.addEventListener("click", onButtonClick);
