9c87ecc60b0d7f3bf6f1c33b5d08d3339b987ede
UHquokka <100435210+UHquokka@users.noreply.github.com>
2022-10-28 15:45:49 +0900

    chating 코너 아이콘 크기 수정

--- js/drawing.js
+++ js/drawing.js
@@ -4,29 +4,19 @@ const ctx = canvas.getContext("2d");
 canvas.width = 600;
 canvas.height = 600;
 
-ctx.rect(200, 200, 80, 200);
-ctx.rect(90, 200, 100, 25);
-ctx.rect(80, 200, 25, 100);
-ctx.rect(380, 125, 25, 100);
-ctx.rect(290, 200, 100, 25);
+const lineWidth = 2;
 
-ctx.fill();
 
-ctx.beginPath();
-ctx.arc(240, 130, 50, 0, 2 * Math.PI);
-ctx.fill();
 
-ctx.beginPath();
-ctx.arc(220, 120, 10, 1 * Math.PI, 0);
-ctx.fillStyle = "yellow";
-ctx.fill();
+function onClick(event) {
+    ctx.beginPath();
+    ctx.moveTo(event.offsetX, event.offsetY);
+    ctx.strokeStyle = "red";
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
 
-ctx.beginPath();
-ctx.arc(260, 120, 10, 1 * Math.PI, 0);
-ctx.fillStyle = "green";
-ctx.fill();
 
-ctx.beginPath();
-ctx.arc(240, 140, 25, 0, 1 * Math.PI);
-ctx.fillStyle = "red";
-ctx.fill();
+}
+
+
+canvas.addEventListener("mousemove", onClick);
