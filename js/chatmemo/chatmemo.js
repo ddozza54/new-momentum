ba60adc68537d82fe652c984084e50d253d0fa97
UHquokka <bananamayong@gmail.com>
2023-03-19 22:45:01 +0900

    calendar API 적용, today's event 생성

---
+++ js/chatmemo/chatmemo.js
@@ -0,0 +1,6 @@
+var calendarEl = document.getElementById('calendar');
+var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+    fixedWeekCount: false,
+});
+calendar.render();
